6e68c28477d059c28c73565a05d195be2e08a791
Vineet Singh <121118445+vinit026@users.noreply.github.com>
2023-06-28 11:45:11 +0530

    app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ Original file is located at
     https://colab.research.google.com/drive/15mKzzQuyzw2qwccUxr2MLHHoMyveedUX
 """
 
-# !pip install -q streamlit
-# !npm install localtunnel
+!pip install -q streamlit
+!npm install localtunnel
 import streamlit as st
 
 # Core Pkgs
